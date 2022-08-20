4df7030841fa9e8b22786cc0a27287ec1e7f2f8d
Abdullah <abdullah.am4853@gmail.com>
2022-08-20 10:56:58 +0300

    Done
    Done

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function BMI(weight, height){
+   let result=weight / (height * height);
+return result;
+}
 
+console.log(BMI(70, 1.7));
+
+function Status(bmi) {
+   if (bmi< 18.5){
+       return("لديك نقص في الوزن")
+   }else if(bmi>=18.5 && bmi<25){
+    return "وزنك صحي";
+   }else if (bmi>=25){
+    return"لديك زيادة في الوزن";
+   }
+}
+
+function calculate(){
+    let weight=document.getElementById("weight").value;
+    let hight=document.getElementById("height").value;
+
+    let bmi = BMI(weight,hight)
+
+    let desc = Status(bmi)
+
+    let div = document.getElementById("result")
+
+    div.innerText =bmi + " == " + desc
+}
